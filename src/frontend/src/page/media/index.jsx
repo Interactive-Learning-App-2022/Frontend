923f112a962df32f55a908b1b4ca37628c079a11
soupnwaffles <KVprivate@hotmail.com>
2022-08-28 14:32:03 -0700

    changes

--- src/frontend/src/page/media/index.jsx
+++ src/frontend/src/page/media/index.jsx
@@ -1,67 +1,53 @@
-<<<<<<< HEAD
-//import React        from 'react';
-//import { Link }     from 'react-router-dom';
-=======
 
->>>>>>> refs/remotes/origin/tilok
-//import ReactPlayer from 'react-player';
+//import { Link }     from 'react-router-dom';
 import MUtil        from 'util/mm.jsx';
 import { VueInReact } from 'vuera';
 
 const _mm           = new MUtil();
 
-<<<<<<< HEAD
-import React, { useRef } from "react"; 
+import React, { useRef} from "react";
 import './index.scss'
 import YouTube from 'react-youtube'; 
 
-export default function App() {
-  return(
-    <div className="App">
-      <div className='left'>
-        <YouTube
-          videoId={"EQKATpGKyKM"}
-          onStateChange={(e) => checkElapsedTime(e)}
-          />
-      </div>
-      <div className='right'>
-        box
-      </div>
-    </div>
-  ); 
-}
+//opt variables for the Youtube player. 
+    const opt = {
+      playerVars:{
+      //Disable keyboard input for video player
+      disablekb: 1,  
+      // Disable full screen icon/button
+      fs: 0,
+      // Disables Youtube logo
+      modestbranding:1,
+      //Allows video to play on mobile without fullscreening
+      playsinline: 1,
+      //Disables suggested videos
+      rel: 0, 
+      //Enables manipulating video player through iframe api
+      enablejsapi: 1
 
-const checkElapsedTime = (e) => {
-  const duration = e.target.getDuration(); 
-  const currentTime = e.target.getCurrentTime(); 
-  console.log(currentTime); 
-}
-=======
-import React, { useRef} from "react";
-import './index.scss'
-import YouTube from 'react-youtube';
-
-
+      }
+    }
+  
 export default function App() {
   return (
     <div className="App">
-      <div className= 'left'>
+      <div className= 'left'> 
       <YouTube className= "left-video" id='video'
-          videoId={"EQKATpGKyKM"}
-          onStateChange={(e) => checkElapsedTime(e)}
-      />
-      </div>
-      <div className='right' >
-        <div className='right-questions'
-        Questions></div>
-      </div>
+        videoId={"EQKATpGKyKM"}
+        opts={opt}
+        onStateChange={(e) => checkElapsedTime(e)}
+    />
     </div>
+        <div className='right' >
+          <div className='right-questions'>
+          Questions</div>
+      </div>
+      </div>
   );
-}
-
-const checkElapsedTime = (e) => {
+  }
+  
+  const checkElapsedTime = (e) => { 
   const duration = e.target.getDuration();
-  const currentTime = e.target.getCurrentTime();
+  const currentTime = e.target.getCurrentTime(); 
   console.log(currentTime);
-};
->>>>>>> refs/remotes/origin/tilok
+  }; 
