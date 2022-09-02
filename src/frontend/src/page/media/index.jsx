bff6712bb2b6bd289617cb47bf8c0849749ea0df
Michaella Magtibay <michaeltmagtibay@gmail.com>
2022-09-01 20:40:33 -0400

    switched react-youtube with react-player

--- src/frontend/src/page/media/index.jsx
+++ src/frontend/src/page/media/index.jsx
@@ -1,53 +1,61 @@
-
 //import { Link }     from 'react-router-dom';
-import MUtil        from 'util/mm.jsx';
-import { VueInReact } from 'vuera';
+import MUtil from "util/mm.jsx";
+import { VueInReact } from "vuera";
 
-const _mm           = new MUtil();
+const _mm = new MUtil();
 
-import React, { useRef} from "react";
-import './index.scss'
-import YouTube from 'react-youtube'; 
+import React, { useRef, useState } from "react";
+import "./index.scss";
+// import YouTube from 'react-youtube';
+import ReactPlayer from "react-player/youtube";
 
-//opt variables for the Youtube player. 
-    const opt = {
-      playerVars:{
-      //Disable keyboard input for video player
-      disablekb: 1,  
-      // Disable full screen icon/button
-      fs: 0,
-      // Disables Youtube logo
-      modestbranding:1,
-      //Allows video to play on mobile without fullscreening
-      playsinline: 1,
-      //Disables suggested videos
-      rel: 0, 
-      //Enables manipulating video player through iframe api
-      enablejsapi: 1
+//opt variables for the Youtube player.
+const opt = {
+  playerVars: {
+    //Disable keyboard input for video player
+    disablekb: 1,
+    // Disable full screen icon/button
+    fs: 0,
+    // Disables Youtube logo
+    modestbranding: 1,
+    //Allows video to play on mobile without fullscreening
+    playsinline: 1,
+    //Disables suggested videos
+    rel: 0,
+    //Enables manipulating video player through iframe api
+    enablejsapi: 1,
+  },
+};
 
-      }
-    }
-  
 export default function App() {
+  const [elapsed, setElapsed] = useState(0);
+  // const [seeking, setSeeking] = useState(false);
+
+  const playerRef = useRef();
+
+  const handleProgress = (state) => {
+    console.log("onProgress", state);
+    setElapsed(state.playedSeconds);
+  };
+
   return (
     <div className="App">
-      <div className= 'left'> 
-      <YouTube className= "left-video" id='video'
-        videoId={"EQKATpGKyKM"}
-        opts={opt}
-        onStateChange={(e) => checkElapsedTime(e)}
-    />
+      <div className="left">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=EQKATpGKyKM"
+          controls={true}
+          onProgress={handleProgress}
+        />
+      </div>
+      <div className="right">
+        <div className="right-questions">Questions</div>
+      </div>
     </div>
-        <div className='right' >
-          <div className='right-questions'>
-          Questions</div>
-      </div>
-      </div>
   );
-  }
-  
-  const checkElapsedTime = (e) => { 
+}
+
+const checkElapsedTime = (e) => {
   const duration = e.target.getDuration();
-  const currentTime = e.target.getCurrentTime(); 
+  const currentTime = e.target.getCurrentTime();
   console.log(currentTime);
-  }; 
+};
