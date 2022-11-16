faebea7910e9fd7d00a33f9925f82774d436bc5b
Tilok Jahan <tilok.jahan@gmail.com>
2022-11-16 17:19:59 -0500

    Courses can be switched back and forth now

--- frontend/src/page/media/index.jsx
+++ frontend/src/page/media/index.jsx
@@ -24,11 +24,14 @@ export default function App() {
   const [volume, setVolume] = useState(); 
   const [playing, setPlaying] = useState(false);
   const [url, setUrl] = useState("https://www.youtube.com/watch?v=EQKATpGKyKM")
+  const[apiCall, setapi] = useState(JSON.parse(
+    '[{"start": 0, "end": 182, "type": "normal", "content": "", "next": 183}, {"start": 183, "end": 289, "type": "walk", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 290, "answer": ["3", "7", "4", "5"]}, {"start": 290, "end": 313, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 315, "fail": 405, "answer": ["2", "6", "3", "8"]}, {"start": 315, "end": 404, "type": "normal", "content": "", "next": 576}, {"start": 405, "end": 575, "type": "normal", "content": "", "next": 576}, {"start": 576, "end": 585, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 0, "fail": 0, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 184}]'
+  ));
 
   // segment jumps work best when both the TS end and next TS start are not the same
-  const apiCall = JSON.parse(
-    '[{"start": 0, "end": 182, "type": "normal", "content": "", "next": 183}, {"start": 183, "end": 289, "type": "walk", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 290, "answer": ["3", "7", "4", "5"]}, {"start": 290, "end": 313, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 315, "fail": 405, "answer": ["2", "6", "3", "8"]}, {"start": 315, "end": 404, "type": "normal", "content": "", "next": 576}, {"start": 405, "end": 575, "type": "normal", "content": "", "next": 576}, {"start": 576, "end": 585, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 0, "fail": 0, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 184}]'
-  );
+  // const apiCall = JSON.parse(
+  //   '[{"start": 0, "end": 182, "type": "normal", "content": "", "next": 183}, {"start": 183, "end": 289, "type": "walk", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 290, "answer": ["3", "7", "4", "5"]}, {"start": 290, "end": 313, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 315, "fail": 405, "answer": ["2", "6", "3", "8"]}, {"start": 315, "end": 404, "type": "normal", "content": "", "next": 576}, {"start": 405, "end": 575, "type": "normal", "content": "", "next": 576}, {"start": 576, "end": 585, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 0, "fail": 0, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 184}]'
+  // );
 
   const player = useRef(null);
 
@@ -109,11 +112,14 @@ export default function App() {
     setVolume(parseFloat(e.target.value)); 
   }
 
+    //switch videos 
   const handleCourseClick = () => {
-    // <ReactPlayer {
-    // setCurrentUrl(videoUrl.current?.player?.player?.player?.currentSrc)
-    // }}
-    setUrl("https://www.youtube.com/watch?v=Vascnx8yk8o");
+    if (url == "https://www.youtube.com/watch?v=EQKATpGKyKM"){
+      setUrl("https://www.youtube.com/watch?v=Vascnx8yk8o");
+    }
+    else{
+      setUrl("https://www.youtube.com/watch?v=EQKATpGKyKM");
+    }
   };
 
   function evaluate() {
@@ -226,11 +232,11 @@ export default function App() {
         <FontAwesomeIcon className="volumehigh" icon={faVolumeHigh}/>
       </div>
       </div> 
-      {/* <div className="courseButton">
+      <div className="courseButton">
           <button onClick={() => handleCourseClick()}>
             Change courses
           </button>
-        </div> */}
+        </div>
         </div>  {/*End of left side */}
       <div className="right"> 
         <div className="right-questions">
