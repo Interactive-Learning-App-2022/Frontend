c357a5beacd20798f4f6244fc399aa08d02c24a3
soupnwaffles <KVprivate@hotmail.com>
2022-11-20 19:19:41 -0800

    few changes to switching courses

--- frontend/src/page/media/index.jsx
+++ frontend/src/page/media/index.jsx
@@ -43,7 +43,7 @@ export default function App() {
 
   // segment jumps work best when both the TS end and next TS start are not the same
   const apiCall = JSON.parse(
-    '[{"start": 0, "end": 183, "type": "normal", "content": "", "next": 187}, {"start": 187, "end": 294, "type": "assess", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 297, "pass":297, "fail": 187, "answer": ["3", "7", "4", "5"]}, {"start": 297, "end": 323, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 414, "fail": 423, "answer": ["2", "6", "3", "8"]}, {"start": 414, "end": 417.1, "type": "normal", "content": "", "next": 598}, {"start": 423, "end": 593, "type": "normal", "content": "", "next": 598 }, {"start": 598, "end": 606, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 414, "fail": 423, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 423}]'
+    '[{"start": 0, "end": 183, "type": "normal", "content": "", "next": 187}, {"start": 187, "end": 294, "type": "assess", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 297, "pass":297, "fail": 187, "answer": ["3", "7", "4", "5"]}, {"start": 297, "end": 323, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 414, "fail": 423, "answer": ["2", "6", "3", "8"]}, {"start": 414, "end": 417, "type": "normal", "content": "", "next": 598}, {"start": 423, "end": 593, "type": "normal", "content": "", "next": 598 }, {"start": 598, "end": 606, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 414, "fail": 423, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 423}]'
   );
 
   const player = useRef(null);
@@ -112,7 +112,7 @@ export default function App() {
       if (result) {
         setCurrentNext(currentTS["pass"]);
         //Put in the start fin
-        if (currentTS["start"] == "598") {
+        if (currentTS["start"] == "598" && subject=="Math") {
           console.log("Finish bool true");
           setFinishBool(true);
           console.log(finishbool);
@@ -154,10 +154,12 @@ export default function App() {
 
   //switch videos
   const handleCourseClick = () => {
-    if (url == "https://www.youtube.com/watch?v=EQKATpGKyKM") {
+    if (url == "https://www.youtube.com/watch?v=EQKATpGKyKM" || url == "https://youtu.be/HJzvuZ56Q9g") {
       setUrl("https://www.youtube.com/watch?v=Vascnx8yk8o");
+      setSubject("English"); 
     } else {
       setUrl("https://www.youtube.com/watch?v=EQKATpGKyKM");
+      setSubject("Math"); 
     }
   };
 
