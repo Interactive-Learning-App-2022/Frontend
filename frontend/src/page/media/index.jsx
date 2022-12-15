dd2694edc7cc7c059fc60dce1b8ce814d2150455
soupnwaffles <KVprivate@hotmail.com>
2022-12-14 17:25:32 -0800

    added isfinalto both JSONs

--- frontend/src/page/media/index.jsx
+++ frontend/src/page/media/index.jsx
@@ -47,7 +47,7 @@ export default function App() {
   const[course, setCourse] = useState("Math");
   // segment jumps work best when both the TS end and next TS start are not the same
   const[apiCall, setapiCall] = useState(JSON.parse(
-    '[{"start": 0, "end": 183, "type": "normal", "content": "", "next": 187}, {"tried": 0,"start": 187, "end": 294, "type": "assess", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 297, "pass":297, "fail": 187, "answer": ["3", "7", "4", "5"]}, {"start": 297, "end": 323, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 414, "fail": 423, "answer": ["2", "6", "3", "8"]}, {"start": 414, "end": 417, "type": "normal", "content": "", "next": 598}, {"start": 423, "end": 593, "type": "normal", "content": "", "next": 598 }, {"start": 598, "end": 606, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 414, "fail": 423, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 423}]'
+    '[{"start": 0, "end": 183, "type": "normal", "content": "", "next": 187}, {"tried": 0,"start": 187, "end": 294, "type": "assess", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 297, "pass":297, "fail": 187, "answer": ["3", "7", "4", "5"]}, {"start": 297, "end": 323, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 414, "fail": 423, "answer": ["2", "6", "3", "8"]}, {"start": 414, "end": 417, "type": "normal", "content": "", "next": 598}, {"start": 423, "end": 593, "type": "normal", "content": "", "next": 598 }, {"start": 598, "end": 606, "isfinal": 1, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 414, "fail": 423, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 423}]'
   ));
 
   const player = useRef(null);
@@ -127,7 +127,7 @@ export default function App() {
         }
         setCurrentNext(currentTS["pass"]);
         //Put in the start fin
-        if (currentTS["start"] == "598" && subject=="Math") {
+        if ("isfinal" in currentTS && subject=="Math") {
           console.log("Finish bool true");
           setFinishBool(true);
           console.log(finishbool);
@@ -215,7 +215,7 @@ export default function App() {
       setSubject("Math"); 
       setapiCall(JSON.parse(
         '[{"start": 0, "end": 183, "type": "normal", "content": "", "next": 187}, {"tried": 0,"start": 187, "end": 294, "type": "assess", "content": "7 + 3 = 10\\n__ + __ = 10\\n\\n5 x 4 = 20\\n__ x __ = 20", "next": 297, "pass":297, "fail": 187,  "answer": ["3", "7", "4", "5"]}, {"start": 297, "end": 323, "type": "assess", "content": "6 + 2 =\\n__ + __ =\\n\\n8 x 3 =\\n__ x __ =", "pass": 414, "fail": 423, "answer": ["2", "6", "3", "8"]}, \
-        {"start": 414, "end": 417, "type": "normal", "content": "", "next": 598}, {"start": 423, "end": 593, "type": "normal", "content": "", "next": 598 }, {"start": 598, "end": 606, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 414, "fail": 423, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 423}]'
+        {"start": 414, "end": 417, "type": "normal", "content": "", "next": 598}, {"start": 423, "end": 593, "type": "normal", "content": "", "next": 598 }, {"start": 598, "end": 606, "isfinal":1, "type": "assess", "content": "4 + 9 = __\\n__ + __ = __\\n\\n7 x 2 = __\\n__ x __ = __\\n\\n5 + 15 = __\\n__ + __ = __", "pass": 414, "fail": 423, "answer": ["13", "9", "4", "13", "14", "2", "7", "14", "20", "15", "5", "20"], "next": 423}]'
       ));
     }
   };
@@ -304,7 +304,7 @@ export default function App() {
             url={url}
             ref={player}
             onProgress={handleProgress}
-            controls={false}
+            controls={true}
             // controls={true}
             playing={playing}
             onPlay={() => {
